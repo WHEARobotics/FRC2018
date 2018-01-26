557c5e5a1cac5fb3f190cdaa7755abdeb979f332
Teo Ippolito <teo.ippolito@whea.net>
2018-01-26 02:24:56 +0000

    Teleop operational, need to customize controller compatible with simulator

---
+++ Scratch/Teo Ippolito Cubert2018/Cubert_001.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python3
+"""
+    WHEA Robotics 3881 code for FRC 2017.
+#test
+"""
+#import robotpy
+import wpilib
+import ctre
+import wpilib.drive
+
+class MyRobot(wpilib.IterativeRobot):
+    
+    def robotInit(self):
+        """
+        This function is called upon program startup and
+        should be used for any initialization code.
+        """
+
+        
+        # Configure shooter motor controller.
+        self.shooter = ctre.wpi_talonsrx.WPI_TalonSRX(5) # Create a CANTalon object.
+        self.shooter.configSelectedFeedbackSensor(ctre.wpi_talonsrx.WPI_TalonSRX.FeedbackDevice.QuadEncoder , 0 , 0) # Choose an encoder as a feedback device.  The default should be QuadEncoder already, but might as well make sure.
+        # I thought the encoder was 20 pulses per revolution per phase, which would require "80" as an argument below, but after trying it, it looks like there are 12.
+        # Setting this parameter changes things so getPosition() returns decimal revolutions, and getSpeed() returns RPM.
+        #self.shooter.configEncoderCodesPerRev(48)
+        # resets shooter position on startup
+        self.shooter.setQuadraturePosition(0, 0)
+        self.shooter.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)# This should change between brake and coast modes.
+        
+
+        self.l_motor1 = ctre.wpi_talonsrx.WPI_TalonSRX(1)
+        self.l_motor2 = ctre.wpi_talonsrx.WPI_TalonSRX(2) 
+        self.l_motor1.setInverted(True)
+        self.l_motor2.setInverted(True)
+        self.r_motor1 = ctre.wpi_talonsrx.WPI_TalonSRX(3)
+        self.r_motor2 = ctre.wpi_talonsrx.WPI_TalonSRX(4)
+        self.r_motor1.setInverted(True)
+        self.r_motor2.setInverted(True)
+        # Configure shooter motor controller.
+         # Create a CANTalon object.
+        self.l_motor1.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
+        self.l_motor1.configSelectedFeedbackSensor(ctre.wpi_talonsrx.WPI_TalonSRX.FeedbackDevice.QuadEncoder, 0, 0)
+        self.l_motor2.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
+        self.l_motor2.configSelectedFeedbackSensor(ctre.wpi_talonsrx.WPI_TalonSRX.FeedbackDevice.QuadEncoder, 0, 0)
+        self.l_motor1.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
+        self.r_motor1.configSelectedFeedbackSensor(ctre.wpi_talonsrx.WPI_TalonSRX.FeedbackDevice.QuadEncoder, 0, 0)
+        self.l_motor2.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
+        self.r_motor2.configSelectedFeedbackSensor(ctre.wpi_talonsrx.WPI_TalonSRX.FeedbackDevice.QuadEncoder, 0, 0)# Choose an encoder as a feedback device.  The default should be QuadEncoder already, but might as well make sure.
+        # I thought the encoder was 20 pulses per revolution per phase, which would require "80" as an argument below, but after trying it, it looks like there are 12.
+        # Setting this parameter changes things so getPosition() returns decimal revolutions, and getSpeed() returns RPM.
+        #self.l_motor1.configEncoderCodesPerRev(48)
+        #self.l_motor2.configEncoderCodesPerRev(48)
+        #self.r_motor1.configEncoderCodesPerRev(48)
+        #self.r_motor2.configEncoderCodesPerRev(48)
+        # resets shooter position on startup
+        self.l_motor1.setQuadraturePosition(0, 0)
+        self.l_motor2.setQuadraturePosition(0, 0)
+        self.r_motor1.setQuadraturePosition(0, 0)
+        self.r_motor2.setQuadraturePosition(0, 0)
+
+        #self.stick = wpilib.Joystick(0)
+        self.l_joy = wpilib.Joystick(0)
+        self.r_joy = wpilib.Joystick(1)
+        self.loader = ctre.wpi_talonsrx.WPI_TalonSRX(5)
+        self.climber = wpilib.Spark(7)
+        self.loader = ctre.wpi_talonsrx.WPI_TalonSRX(6)
+        self.drive = wpilib.RobotDrive(self.l_motor1 , self.l_motor2 , self.r_motor1 , self.r_motor2)
+        self.counter = 0
+        self.mode = 0
+        #wpilib.CameraServer.launch()
+        #IP for camera server: http://10.38.81.101:1181/
+        
+        
+        
+
+    def autonomousInit(self):
+        """This function is run once each time the robot enters autonomous mode."""
+        self.auto_loop_counter = 0
+        self.shooter.setQuadraturePosition(0, 0)
+        self.l_motor1.setQuadraturePosition(0, 0)
+        self.r_motor1.setQuadraturePosition(0, 0)
+        self.l_motor1.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Brake)
+        self.r_motor1.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Brake)
+        self.l_motor2.setQuadraturePosition(0, 0)
+        self.r_motor2.setQuadraturePosition(0, 0)
+        self.l_motor2.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Brake)
+        self.r_motor2.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Brake)
+
+        
+        #self.gameData = wpilib.DriverStation.getInstance().getGameSpecificMessage()
+        #self.logger.info(self.gameData)
+        self.gameData = 'LRL'
+
+		
+    
+
+    def autonomousPeriodic(self):
+        
+        if(self.gameData[0] == 'L'):    	
+            self.AutoPL(self)
+            
+        else: 
+            self.AutoPR(self)
+
+    def AutoPL(self):
+
+        if self.r_motor1.getPosition()> -2000 and self.auto_loop_counter <200:
+            self.drive.drive(-0.5,0)
+
+        else:
+            self.drive.drive(0,0)
+
+        self.auto_loop_counter +=1
+
+        if self.auto_loop_counter % 50 == 0:
+            # Uncomment whichever line you want to use.  Need to have a shooter to use the second one.
+            print(self.auto_loop_counter, ' pos: ', self.l_motor1.getPosition() , self.r_motor1.getPosition())
+        #This counter runs 50 times a second
+
+    def AutoPR(self):
+        
+        if self.r_motor1.getPosition()> -2000 and self.auto_loop_counter <200:
+            self.drive.drive(-0.5,0)
+
+        else:
+            self.drive.drive(0,0)
+
+        self.auto_loop_counter +=1
+
+        if self.auto_loop_counter % 50 == 0:
+            # Uncomment whichever line you want to use.  Need to have a shooter to use the second one.
+            print(self.auto_loop_counter, ' pos: ', self.l_motor1.getPosition() , self.r_motor2.getPosition())
+        #This counter runs 50 times a second
+            
+            
+            
+            
+
+
+    def teleopInit(self):
+        #resets printed shooter position on enable
+        self.shooter.setQuadraturePosition(0, 0)
+        self.l_motor1.setQuadraturePosition(0, 0)
+        self.r_motor1.setQuadraturePosition(0, 0)
+        self.l_motor2.setQuadraturePosition(0, 0)
+        self.r_motor2.setQuadraturePosition(0, 0)
+        self.tele_counter = 0
+        self.auto_loop_counter = 0
+        self.l_motor1.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
+        self.r_motor1.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
+        self.l_motor2.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
+        self.r_motor2.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
+        
+
+    def teleopPeriodic(self):
+        """This function is called periodically during operator control."""
+        
+        self.drive.tankDrive(self.l_joy.getRawAxis(1) , self.r_joy.getRawAxis(1))
+        
+        if self.l_joy.getRawButton(2) or self.r_joy.getRawButton(2):
+            self.climber.set(1)
+        else:
+            self.climber.set(0)
+
+        if self.l_joy.getRawButton(3) or self.r_joy.getRawButton(3):
+            self.loader.set(-0.5)
+        else:
+            self.loader.set(0)
+            
+
+        # Pulling the trigger starts the shooting/loading process, releasing stops it.
+        if self.l_joy.getRawButton(1):
+            self.shooter.set(1)
+            self.auto_loop_counter += 1 # Setting it to nonzero starts it up; only increments when trigger held.
+        else:
+            self.auto_loop_counter = 0 # setting to zero stops below.
+
+        # Now do things based on the value of the counter.
+        if self.auto_loop_counter == 0:
+            self.shooter.set(0) # If zero, we are stopped.
+            self.loader.set(0)
+        elif self.auto_loop_counter > 0 and self.auto_loop_counter <= 100:
+        # First two seconds shooter only is on.  Technically, we would only have to set when entering this condition, but this makes it clear.
+             self.shooter.set(1)
+        else:
+        # Executed if self.auto_loop_counter is > 100, or trigger held longer than 2 seconds.
+             self.shooter.set(1)
+             self.loader.set(0.5)
+        
+        self.auto_loop_counter +=1
+
+        """
+        if self.mode == 0:
+            self.release.set(1)
+            if self.r_joy.getRawButton(1):
+                self.mode = 1
+                self.tele_counter = 0
+
+        elif self.mode == 1:
+            self.release.set(0.5)
+            if self.tele_counter < 100:
+                self.mode = 0
+
+        self.tele_counter += 1
+        """
+        
+        self.counter += 1
+        if self.counter % 90 == 0:
+            # Uncomment whichever line you want to use.  Need to have a shooter to use the second one.
+            print(self.counter, ' pos: ', self.l_motor1.getQuadraturePosition() , self.r_motor1.getQuadraturePosition())
+            
+
+            """
+            print(self.counter)
+            """
+            print(self.counter, ' axis: ', self.l_joy.getRawAxis(2) and self.r_joy.getRawAxis(2), ' pos: ', self.loader.getQuadraturePosition(), ' rpm: ', self.loader.getQuadratureVelocity())
+            
+
+    def testPeriodic(self):
+        """This function is called periodically during test mode."""
+        wpilib.LiveWindow.run()
+        
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
